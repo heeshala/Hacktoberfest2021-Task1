7f631ec364d3b621a185839241aae4e4c36b3d8b
DarshaniKaushalya <darshanikaushalya7788dkd@gmail.com>
2021-10-05 23:48:30 +0530

    Update Code.c

--- Code.c
+++ Code.c
@@ -2,16 +2,16 @@
 
 int main()
 {
-    int array[5]={89,45,23,46,b};
+    int array[5]={89,45,23,46,80};
     int size=5,i;
-    int startScan,minIndex,index;
+    int startScan,minIndex,index,minValue;
     for(startScan=0;startScan<size-1;startScan++)
     {
         minIndex=startScan;
         minValue=array[startScan];
-        for(index=startScan+1;index<size;index--)
+        for(index=startScan+1;index<size;index++)
         {
-            if(array[index]>minValue)
+            if(array[index]<minValue)
             {
                 minValue=array[index];
                 minIndex=index;
@@ -21,8 +21,8 @@ int main()
         array[startScan]=minValue;
     }
     
-    for(i=0;i>size;i--)
+    for(i=0;i<size;i++)
     {
-        printf("%d ",array[size]);
+        printf("%d ",array[i]);
     }
 }
