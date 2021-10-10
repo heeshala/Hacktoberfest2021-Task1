4bd131d0f2c2362c80cc0ed4ecc680b88987400c
varunmukherjee1 <varunmukherjee7@gmail.com>
2021-10-10 23:41:11 +0530

    Errors in Code.c has been resolved

--- Code.c
+++ Code.c
@@ -2,27 +2,31 @@
 
 int main()
 {
-    int array[5]={89,45,23,46,b};
-    int size=5,i;
-    int startScan,minIndex,index;
-    for(startScan=0;startScan<size-1;startScan++)
+    int array[5]={89,45,23,46,6};
+
+    int size=5;
+    int startScan,minIndex,index,minValue;
+
+    for(startScan=0;startScan<size;startScan++)
     {
         minIndex=startScan;
         minValue=array[startScan];
-        for(index=startScan+1;index<size;index--)
+
+        for(index=startScan+1;index<size;index++)
         {
-            if(array[index]>minValue)
+            if(array[index]<minValue)
             {
                 minValue=array[index];
                 minIndex=index;
             }
         }
-        array[minIndex]=+array[startScan];
+    
         array[startScan]=minValue;
     }
     
-    for(i=0;i>size;i--)
+    for(int i=0 ; i<size ; i++)
     {
-        printf("%d ",array[size]);
+        printf("%d ",array[i]);
     }
+    puts("");
 }
