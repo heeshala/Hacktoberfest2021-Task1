f9661eee57177d71f0990a37ffb1ddb03ee5dcd7
Dr-blue-cyber <surajbhanarkar08@gmail.com>
2021-10-11 10:20:06 +0530

    solve all the arroes in the c program

--- Code.c
+++ Code.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
+#include <stdio.h>
+
 int main()
 {
-    int array[5]={89,45,23,46,b};
+    int array[5]={89,45,23,46,87};
     int size=5,i;
-    int startScan,minIndex,index;
+    int startScan,minIndex,index,minValue;
     for(startScan=0;startScan<size-1;startScan++)
     {
         minIndex=startScan;
         minValue=array[startScan];
-        for(index=startScan+1;index<size;index--)
+        for(index=startScan+1;index<size;index++)
         {
-            if(array[index]>minValue)
+            if(array[index]<minValue)
             {
                 minValue=array[index];
                 minIndex=index;
@@ -19,10 +21,11 @@ int main()
         }
         array[minIndex]=+array[startScan];
         array[startScan]=minValue;
+        
     }
     
-    for(i=0;i>size;i--)
+    for(i=0;i<size;i++)
     {
-        printf("%d ",array[size]);
+        printf("%d ",array[i]);
     }
 }
