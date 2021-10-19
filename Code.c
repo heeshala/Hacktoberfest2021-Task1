cd2d51dc6c94361686b431df5ecd0ad4119ecdc9
Nihara-Dasanayaka <73481471+Nihara-Dasanayaka@users.noreply.github.com>
2021-10-20 01:31:37 +0530

    Update Code.c

--- Code.c
+++ Code.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int array[5]={89,45,23,46,b};
+    int array[5]={89,45,23,46,15};
     int size=5,i;
     int startScan,minIndex,index;
     for(startScan=0;startScan<size-1;startScan++)
